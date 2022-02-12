f3c42dc128480521cb53be63e1b351d034381389
Respah-Nafula <nafularespah@gmail.com>
2022-02-12 08:38:37 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var fullnames="Simon keith Masika"
+    println(fullnames)
+    var age="18"
+    println(age)
+    var phonenumber="0742355265"
+    println(phonenumber)
+    var weight="57kg"
+    println(weight)
+            var studentkenyan= true
+            var studentnotkenyan= false
+    println(studentkenyan) // output true
+    println(studentnotkenyan) //output false
+
+
+}
+
